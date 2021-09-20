bd498726853be04adb1f588e7eca896bd701f80f
Ana Ortez <anaortez23@gmail.com>
2021-09-20 15:58:53 -0400

    is it alive?

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deployment is happening here! 😊
+          It's alive! 🎃
         </p>
         {/* <a
           className="App-link"
